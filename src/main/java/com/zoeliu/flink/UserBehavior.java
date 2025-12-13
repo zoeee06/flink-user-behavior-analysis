ece6189c946433a6ffac37909d3e2129cb7cbbce
Zoe Liu <107218898+zoeee06@users.noreply.github.com>
2025-12-13 18:36:49 -0500

    Create UserBehavior

---
+++ src/main/java/com/zoeliu/flink/UserBehavior.java
@@ -0,0 +1,31 @@
+package com.zoeliu.flink;
+
+public class UserBehavior {
+    public String userId;
+    public String itemId;
+    public String categoryId;
+    public String behavior;
+    public Long timestamp;
+
+    public UserBehavior() {
+    }
+
+    public UserBehavior(String userId, String itemId, String categoryId, String behavior, Long timestamp) {
+        this.userId = userId;
+        this.itemId = itemId;
+        this.behavior = behavior;
+        this.categoryId = categoryId;
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "UserBehavior{" +
+                "userId='" + userId + '\'' +
+                ", itemId='" + itemId + '\'' +
+                ", categoryId='" + categoryId + '\'' +
+                ", behavior='" + behavior + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
